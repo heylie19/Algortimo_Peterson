1fb878813319be2f01f7d1eda54501005424d73b
heylie19 <106571599+heylie19@users.noreply.github.com>
2024-04-21 17:08:31 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Peterson peterson = new Peterson();
-        Thread process0 = new Thread(new Process(peterson, 0));
-        Thread process1 = new Thread(new Process(peterson, 1));
-        process0.start();
-        process1.start();
-    }
-}
